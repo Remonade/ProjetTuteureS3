82d5acb5354ae964a6f5a0bfcfc488b91d9eddb6
Thrandread <thrandread@gmail.com>
2015-11-01 00:47:58 +0100

    Add Test folder + Player/Item/Weapon classes

---
+++ Logic/Item.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author Flavien
+ */
+class Item {
+    private String name;
+    private int strengthBuff;
+    private int defenseBuff;
+    private int agilityBuff;
+    private int maxHPBuff;
+    
+    public Item(){
+        this.name="Useless item";
+        this.strengthBuff=0;
+        this.defenseBuff=0;
+        this.agilityBuff=0;
+        this.maxHPBuff=0;
+    }
+    
+    public Item(String name, int strengthBuff, int defenseBuff, int agilityBuff, int maxHPBuff){
+        this.name=name;
+        this.strengthBuff=strengthBuff;
+        this.defenseBuff=defenseBuff;
+        this.agilityBuff=agilityBuff;
+        this.maxHPBuff=maxHPBuff;
+    }
+    
+    public String toString(){
+        return "name : "+this.name+"\n"+
+               "Strength buff : "+this.strengthBuff+"\n"+
+               "Defense buff : "+this.defenseBuff+"\n"+
+               "Agility buff : "+this.agilityBuff+"\n"+
+               "Max health buff : "+this.maxHPBuff;
+    }
+    
+    //getters
+    public String getName(){
+        return this.name;
+    }
+    public int getStrengthBuff(){
+        return this.strengthBuff;
+    }
+    public int getDefenseBuff(){
+        return this.defenseBuff;
+    }
+    public int getAgilityBuff(){
+        return this.agilityBuff;
+    }
+    public int getMaxHPBuff(){
+        return this.maxHPBuff;
+    }
+    
+    //Setters
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setStrengthBuff(int strengthBuff){
+        this.strengthBuff=strengthBuff;
+    }
+    public void setDefenseBuff(int defenseBuff) {
+        this.defenseBuff = defenseBuff;
+    }
+    public void setAgilityBuff(int agilityBuff) {
+        this.agilityBuff = agilityBuff;
+    }
+    public void setMaxHPBuff(int maxHPBuff) {
+        this.maxHPBuff = maxHPBuff;
+    }
+}
---
+++ Logic/Weapon.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author Flavien
+ */
+class Weapon {
+    private String name;
+    private int damage;
+    private int type; //0:melee, 1:ranged
+        
+    public Weapon() {
+        this.name="unknown laser sword";
+        this.damage=5;
+        this.type=0;
+    }
+    public Weapon(String name, int damage, int type){
+        this.name=name;
+        this.damage=damage;
+        this.type=type;
+    }
+    
+    public String toString(){
+        String returningString = "Name : "+this.name+"\n"+
+                                "Damage : "+this.damage+"\n"+
+                                "Type : ";
+        if (this.type==0){
+            returningString = returningString+"melee";
+        }
+        else{
+            returningString = returningString+"ranged";
+        }
+        return returningString;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    public int getDamage(){
+        return this.damage;
+    }
+    public int getType(){
+        return this.type;
+    }
+    
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setDamage(int damage){
+        this.damage=damage;
+    }
+    public void setType(short type){
+        this.type=type;
+    }
+    
+}
