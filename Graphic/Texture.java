3d65cd4ae548e47502339c7f3b4af56d416650c6
Daratrixx <zeratul_du_38@hotmail.fr>
2015-11-10 15:49:05 +0100

    Update Texture.java

--- Graphic/Texture.java
+++ Graphic/Texture.java
@@ -1,56 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Graphic;
 
-import java.nio.IntBuffer;
+import java.nio.ByteBuffer;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.*;
 
 public class Texture {
 	public Texture(String path) {
-		IntBuffer texture=IntBuffer.allocate(64);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
-		texture.put(0x88);texture.put(0x88);texture.put(0x88);texture.put(0x88);
+		ByteBuffer texture=ByteBuffer.allocate(64);
+		for(int i=0;i<64;i++)
+			texture.put((byte)0x88);
 		texture.flip();
 		
 		m_ID=glGenTextures();
+		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D,m_ID);
-		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 		glTexImage2D (
 		GL_TEXTURE_2D, 	//Type : texture 2D
 		0, 	//Mipmap : aucun
 		GL_RGBA, 	//Couleurs : 4
-		4, 	//Largeur : 2
-		4, 	//Hauteur : 2
+		4, 	//Largeur : 4
+		4, 	//Hauteur : 4
 		0, 	//Largeur du bord : 0
 		GL_RGBA, 	//Format : RGBA
-		GL_INT, 	//Type des couleurs
+		GL_UNSIGNED_BYTE, 	//Type des couleurs
 		texture); 	//Addresse de l'image
 		glBindTexture(GL_TEXTURE_2D,0);
 	}
 	public int getID() {
 		return m_ID;
+	}
+	public void bind() {
+		glEnable(GL_TEXTURE);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D,m_ID);
+	}
+	public void unbind() {
+		glBindTexture(GL_TEXTURE_2D,0);
+		glDisable(GL_TEXTURE);
 	}
 	public void destroy() {
 		glDeleteTextures(m_ID);
