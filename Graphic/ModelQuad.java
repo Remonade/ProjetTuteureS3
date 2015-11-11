12ac1b8f3b762e345f039f841bb1ccbf37114e2f
Daratrixx <zeratul_du_38@hotmail.fr>
2015-11-11 22:09:24 +0100

    Update ModelQuad.java
    Ajout d'une méthode draw(pos,size) permettant de d'indiquer la taille du réctangle à afficher. La taille (et la position) sont passés en paramètre de la fonction en utilisant des Vector2f.

--- Graphic/ModelQuad.java
+++ Graphic/ModelQuad.java
@@ -3,9 +3,14 @@ package Graphic;
  * TODO: javadoc
 */
 import java.nio.FloatBuffer;
+import math.Vector2f;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glScalef;
+import static org.lwjgl.opengl.GL11.glTranslatef;
 
 public class ModelQuad extends Model{
-
 	public ModelQuad(float r,float g,float b) {
 		super();
 		System.out.println("New ModelQuad instance");
@@ -15,20 +20,20 @@ public class ModelQuad extends Model{
 		m_colors=FloatBuffer.allocate(m_verticeCount*3);
 		
 		// init vertice cord
-		m_vertice.put(m_size*-0.5f);m_vertice.put(m_size*-0.5f);
-		m_vertice.put(m_size*+0.5f);m_vertice.put(m_size*-0.5f);
-		m_vertice.put(m_size*+0.5f);m_vertice.put(m_size*+0.5f);
-		m_vertice.put(m_size*-0.5f);m_vertice.put(m_size*-0.5f);
-		m_vertice.put(m_size*-0.5f);m_vertice.put(m_size*+0.5f);
-		m_vertice.put(m_size*+0.5f);m_vertice.put(m_size*+0.5f);
+		m_vertice.put(-1f);m_vertice.put(-1f);
+		m_vertice.put(+1f);m_vertice.put(-1f);
+		m_vertice.put(+1f);m_vertice.put(+1f);
+		m_vertice.put(-1f);m_vertice.put(-1f);
+		m_vertice.put(-1f);m_vertice.put(+1f);
+		m_vertice.put(+1f);m_vertice.put(+1f);
 		
 		// init textures cord
 		m_textures.put(0);m_textures.put(0);
-		m_textures.put(1);m_textures.put(0);
-		m_textures.put(1);m_textures.put(1);
+		m_textures.put(2.0f);m_textures.put(0);
+		m_textures.put(2.0f);m_textures.put(2.0f);
 		m_textures.put(0);m_textures.put(0);
-		m_textures.put(0);m_textures.put(1);
-		m_textures.put(1);m_textures.put(1);
+		m_textures.put(0);m_textures.put(2.0f);
+		m_textures.put(2.0f);m_textures.put(2.0f);
 		
 		// init colors
 		for(int i=0;i<m_verticeCount;i++) {
@@ -41,5 +46,18 @@ public class ModelQuad extends Model{
 		m_vertice.flip();
 		m_textures.flip();
 		m_colors.flip();
+	}
+	public void draw(Vector2f pos,Vector2f size) {
+		startRender(pos,size);
+		render();
+		endRender();
+	}
+	public void startRender(Vector2f pos,Vector2f size) {
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glTranslatef(pos.x,pos.y,0);
+		glScalef(size.x,size.y,0);
+		if(m_texture!=null)
+			m_texture.bind(); // bind texture
 	}
 }
