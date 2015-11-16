18543a85c9224e0ee9840a8b83d8dc50be2b3995
Daratrixx <zeratul_du_38@hotmail.fr>
2015-11-16 21:32:53 +0100

    Update GraphicMain.java

--- Graphic/GraphicMain.java
+++ Graphic/GraphicMain.java
@@ -1,210 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package Graphic;
 
-import Logic.Ennemi;
-import Logic.Player;
-import math.Vector2f;
-import org.lwjgl.Sys;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-
-import org.lwjgl.glfw.GLFWKeyCallback;
-
+import Logic.Entity;
+import java.util.ArrayList;
+import java.util.HashMap;
 import static org.lwjgl.glfw.GLFW.*;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL30.*;
-import static org.lwjgl.system.MemoryUtil.*;
-
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class GraphicMain {
- 
-    // We need to strongly reference callback instances.
-    private GLFWErrorCallback errorCallback;
-    private GLFWKeyCallback   keyCallback;
- 
-    // The window handle
-    private long window;
-        
-	private Player m_player;
-        private Ennemi m_ennemi;
-        private Ennemi m_ennemi2;
-        
-	private Shader m_shader;
-	private int m_VAO;
-	private float m_mouseX=0;
-	private float m_mouseY=0;
-	private Camera2D m_camera;
-	private int WIDTH = 800;
-	private int HEIGHT = 600;
-	
-	private boolean LEFT;
-	private boolean RIGHT;
-	private boolean UP;
-	private boolean DOWN;
-	
-    public void run() {
-        System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
-        try {
-            init();
-            loop();
-			quit();
-            // Release window and window callbacks
-            glfwDestroyWindow(window);
-            keyCallback.release();
-        } finally {
-            // Terminate GLFW and release the GLFWErrorCallback
-            glfwTerminate();
-            errorCallback.release();
-        }
-    }
-    private void init() {
+	public static void init(int w,int h) {
 		initGL();
-		initWindow();
-		initKeyCallback();
-		initData();
-        // Make the window visible
-        glfwShowWindow(window);
-		//GLContext.createFromCurrent();
-		System.out.println("Init successful");
-    }
-		private void initGL() {
-			// Setup an error callback. The default implementation
-			// will print the error message in System.err.
-			glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
-
-			// Initialize GLFW. Most GLFW functions will not work before doing this.
-			if ( glfwInit() != GL11.GL_TRUE )
-				throw new IllegalStateException("Unable to initialize GLFW");
-		}
-		private void initWindow() {
-			// Configure our window
-			glfwDefaultWindowHints(); // optional, the current window hints are already the default
-			glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden after creation
-			glfwWindowHint(GLFW_DOUBLE_BUFFER, GL_TRUE);
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-			//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
-			//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-			// Create the window
-			window = glfwCreateWindow(WIDTH, HEIGHT, "Shader", NULL, NULL);
-			if ( window == NULL )
-				throw new RuntimeException("Failed to create the GLFW window");
-
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-			// Center our window
-			glfwSetWindowPos(
-				window,
-				(vidmode.getWidth() - WIDTH) / 2,
-				(vidmode.getHeight() - HEIGHT) / 2
-			);
-
-			// Make the OpenGL context current
-			glfwMakeContextCurrent(window);
-			//GLContext.createFromCurrent();
-			// Enable v-sync
-			glfwSwapInterval(1);
-			GL.createCapabilities();
-			glEnable(GL_TEXTURE_2D);
-			glEnable(GL_BLEND);
-			//glEnable(GL_DEPTH_TEST);
-			glHint(GL_PERSPECTIVE_CORRECTION_HINT,GL_NICEST);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		}
-		private void initKeyCallback() {
-			// Setup a key callback. It will be called every time a key is pressed, repeated or released.
-			glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
-				@Override
-				public void invoke(long window, int key, int scancode, int action, int mods) {
-                                    if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                                            glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
-                                    if ( key == GLFW_KEY_LEFT && action == GLFW_PRESS )
-                                            LEFT=true;
-                                    if ( key == GLFW_KEY_LEFT && action == GLFW_RELEASE )
-                                            LEFT=false;
-                                    if ( key == GLFW_KEY_RIGHT && action == GLFW_PRESS )
-                                            RIGHT=true;
-                                    if ( key == GLFW_KEY_RIGHT && action == GLFW_RELEASE )
-                                            RIGHT=false;
-                                    if ( key == GLFW_KEY_UP && action == GLFW_PRESS )
-                                            UP=true;
-                                    if ( key == GLFW_KEY_UP && action == GLFW_RELEASE )
-                                            UP=false;
-                                    if ( key == GLFW_KEY_DOWN && action == GLFW_PRESS )
-                                            DOWN=true;
-                                    if ( key == GLFW_KEY_DOWN && action == GLFW_RELEASE )
-                                            DOWN=false;
-				}
-			});
-		}
-		private void initData() {
-			// GL allocation
-			m_VAO = glGenVertexArrays();
-			glBindVertexArray(m_VAO);
-			// Camera setting
-			m_camera=new Camera2D();
-			m_camera.setBound(1.5f, -1.5f, -1.5f, 1.5f);
-			// matrix setting
-			// Data declaration
-			m_player=new Player(new ModelPlayer());
-                        
-                        m_ennemi=new Ennemi(new ModelEnnemi(0.6f, 0.4f, 0.2f), 0, 1);
-                        m_ennemi2=new Ennemi(new ModelEnnemi(0.2f, 0.4f, 0.6f), 0, -1);
-                        
-			m_shader=new Shader();
-		}
-    private void loop() {
-        // This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
- 
-        // Set the clear color
-        glClearColor(0.25f, 0.25f, 0.25f, 0.0f);
-		
-		float angle=0.0f;
-        while(glfwWindowShouldClose(window) == GL_FALSE) {
-			// draw to screen.
-			angle+=0.05;
-			display(angle);
-			
-            // Poll for window events. The key callback above will only be
-            // invoked during this call.
-            glfwPollEvents();
-			if(UP) m_mouseY+=8.0f/(float)HEIGHT;
-			if(DOWN) m_mouseY-=8.0f/(float)HEIGHT;
-			if(LEFT) m_mouseX-=8.0f/(float)WIDTH;
-			if(RIGHT) m_mouseX+=8.0f/(float)WIDTH;
-        }
-    }
-		private void display(float angle) {
-			glClear(GL_COLOR_BUFFER_BIT); // clear the framebuffer
-			m_camera.setPos(m_mouseX,m_mouseY);
-			m_camera.useCamera();
-                        
-                        Vector2f current = new Vector2f(m_mouseX,m_mouseY);
-                        Vector2f size = new Vector2f(1,1);
-                        
-                        m_player.m_coordinates = current;
-			m_player.m_model.draw(m_player.m_coordinates, size);
-                        
-                        m_ennemi.walk(m_player);
-                        m_ennemi.rotate();
-                        m_ennemi.m_model.draw(m_ennemi.m_coordinates, size);
-                        m_ennemi2.walk(m_player);
-                        m_ennemi2.rotate();
-                        m_ennemi2.m_model.draw(m_ennemi2.m_coordinates, size);
-                        
-			//m_model.draw(m_shader,m_modelView,m_projection);
-			glfwSwapBuffers(window); // swap the color buffers
-		}
-	private void quit() {
-		glDeleteVertexArrays(m_VAO);
-		m_shader.destroy();
+		initWindow(w,h);
+		initShader();
+		initTexture();
+		initModel();
+		m_camera.setBound(10.0f, -1.0f, -1.5f, 1.5f);
 	}
-    public static void main(String[] args) {
-        new GraphicMain().run();
-    }
+	private static void initGL() {
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if ( glfwInit() != GL11.GL_TRUE )
+			throw new IllegalStateException("Unable to initialize GLFW");
+	}
+	private static void initWindow(int w, int h) {
+		HEIGHT=h;
+		WIDTH=w;
+		// Configure our window
+		glfwDefaultWindowHints(); // optional, the current window hints are already the default
+		glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden after creation
+		glfwWindowHint(GLFW_DOUBLE_BUFFER, GL_TRUE);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+		//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+		// Create the window
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Shader", NULL, NULL);
+		if ( window == NULL )
+			throw new RuntimeException("Failed to create the GLFW window");
+
+		// Get the resolution of the primary monitor
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		// Center our window
+		glfwSetWindowPos(
+			window,
+			(vidmode.getWidth() - WIDTH) / 2,
+			(vidmode.getHeight() - HEIGHT) / 2
+		);
+
+		// Make the OpenGL context current
+		glfwMakeContextCurrent(window);
+		//GLContext.createFromCurrent();
+		// Enable v-sync
+		glfwSwapInterval(1);
+		GL.createCapabilities();
+		//glEnable(GL_TEXTURE_2D);
+		glEnable(GL_BLEND);
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LESS);
+		glDepthMask(true);
+		glHint(GL_PERSPECTIVE_CORRECTION_HINT,GL_NICEST);
+	}
+	private static void initShader() {
+		
+	}
+	private static void initTexture() {
+		
+	}
+	private static void initModel() {
+		m_models.put("default",new ModelQuad(1,1,1));
+		m_models.put("white",m_models.get("default"));
+		m_models.put("red",new ModelQuad(1,0,0));
+		m_models.put("green",new ModelQuad(0,1,0));
+		m_models.put("blue",new ModelQuad(0,0,1));
+	}
+	public static Model getModel(String ref) {
+		Model temp=m_models.get(ref);
+		if(temp==null)
+			return m_models.get("default");
+		return temp;
+	}
+	public static void display(ArrayList<Entity> entities) {
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+		m_camera.useCamera();
+		LAYER=0;
+		for(Entity i:entities) {
+			i.draw();
+			//LAYER++;
+		}
+		glfwSwapBuffers(window); // swap the color buffers
+	}
+	public static void clear() {
+		m_models.clear();
+	}
+	public static HashMap<String,Model> m_models=new HashMap();
+	public static Camera2D m_camera=new Camera2D();
+	public static long window;
+	public static int HEIGHT, WIDTH;
+	public static int LAYER=-50;
 }
