b24f04f1727430f4b7decafdd3f35c2374b7f86b
Daratrixx <zeratul_du_38@hotmail.fr>
2015-11-19 18:19:59 +0100

    Update PhysicMain.java

--- Physic/PhysicMain.java
+++ Physic/PhysicMain.java
@@ -2,67 +2,121 @@ package Physic;
 
 import Logic.Entity;
 import Logic.EntityDynamic;
+import Logic.Logic;
 import java.util.ArrayList;
 import Maths.Vector2f;
 
 public class PhysicMain {
     
-    boolean collision;
+	boolean collision;
     
-    public static void update(ArrayList<Entity> entities){
-        for(int i=0; i<entities.size(); i++){
-            Entity temp=entities.get(i);
-            if (temp instanceof EntityDynamic) {
-                EntityDynamic temp2=(EntityDynamic)temp;
-                temp2.addSpeed(0f,Gravity);
-                Entity futurY=new Entity();
-                futurY.setSize(temp2.getSize());
-                futurY.setPos(temp2.getPos().add(new Vector2f(0,temp2.getSpeed().y)));
-                boolean isColliding = false;
-                for(int j=0;j<entities.size() && !isColliding; j++){
-                    if(i!=j)
-                    isColliding=collisionEE(futurY, entities.get(j));
-                }
-                if(!isColliding)
-                    temp.setPos(futurY.getPos());
-                else
-                    temp2.setSpeed(temp2.getSpeed().x,0);
-                
-                Entity futurX=new Entity();
-                futurX.setSize(temp2.getSize());
-                futurX.setPos(temp2.getPos().add(new Vector2f(temp2.getSpeed().x,0)));
-                isColliding = false;
-                for(int j=0;j<entities.size() && !isColliding; j++){
-                    if(i!=j)
-                    isColliding=collisionEE(futurX, entities.get(j));
-                }
-                if(!isColliding)
-                    temp.setPos(futurX.getPos());
-                else
-                    temp2.setSpeed(0,temp2.getSpeed().y);
-            }
-        }
-        
-    }
+	public static void update(){
+		ArrayList<EntityDynamic> entities=Logic.getDynamic();
+		for (int i=0;i<entities.size();i++) {
+			EntityDynamic dyn=entities.get(i);
+			dyn.update();
+			adjustX(dyn,Logic.getEntity());
+			adjustY(dyn,Logic.getEntity());
+			if(dyn.getContact(EntityDynamic.CONTACT_DOWN))
+				dyn.setSpeed(dyn.getSpeed().x*0.9f,dyn.getSpeed().y);
+			if(dyn.getContact(EntityDynamic.CONTACT_LEFT) || dyn.getContact(EntityDynamic.CONTACT_RIGHT))
+				dyn.setSpeed(dyn.getSpeed().x,dyn.getSpeed().y*0.9f);
+		}
+	}
     
-    public static boolean collisionEE(Entity ent1, Entity ent2){
-        Vector2f pos1 = ent1.getPos();
-        Vector2f pos2 = ent2.getPos();
-        Vector2f size1 = ent1.getSize();
-        Vector2f size2 = ent2.getSize();
+	public static boolean collisionEE(Entity ent1, Entity ent2){
+		Vector2f pos1 = ent1.getPos();
+		Vector2f pos2 = ent2.getPos();
+		Vector2f size1 = ent1.getSize();
+		Vector2f size2 = ent2.getSize();
 
-        if(pos1.x-size1.x > pos2.x+size2.x || //gauche1 > droite2
-           pos1.x+size1.x < pos2.x-size2.x || //droite1 < gauche2
-           pos1.y-size1.y > pos2.y+size2.y || //bas1 > haut2
-           pos1.y+size1.y < pos2.y-size2.y){ //haut1 < bas2
-           System.out.println("OUT");
-           return false;
-        }
-        System.out.println("IN");
-        return true;
-       
-    }
-    
-    private static final float Gravity = -0.0005f;
-            
+		if( pos1.x-size1.x > pos2.x+size2.x || //gauche1 > droite2
+			pos1.x+size1.x < pos2.x-size2.x || //droite1 < gauche2
+			pos1.y-size1.y > pos2.y+size2.y || //bas1 > haut2
+			pos1.y+size1.y < pos2.y-size2.y){ //haut1 < bas2
+			return false;
+		}
+		return true;
+	}
+	public static void adjustY(EntityDynamic dyn,ArrayList<Entity> entities) {
+		Entity futurY=new Entity();
+		futurY.setData(dyn.getData());
+		Vector2f speed=new Vector2f(0,dyn.getSpeed().y);
+		futurY.setPos(dyn.getPos().add(speed));
+		
+		int i=entities.indexOf(dyn);
+		
+		boolean isColliding = false;
+		Entity collider=null;
+		
+		for(int j=0;j<entities.size() && !isColliding;j++){
+			if(i!=j && entities.get(j).getCollide()) {
+				collider=entities.get(j);
+				isColliding=collisionEE(futurY, collider);
+				while(isColliding && Math.abs(speed.y)>0.005f) {
+					if(dyn.getPos().y > collider.getPos().y) {
+						dyn.setContact(EntityDynamic.CONTACT_DOWN,true);
+						dyn.setContact(EntityDynamic.CONTACT_UP,false);
+					} else {
+						dyn.setContact(EntityDynamic.CONTACT_DOWN,false);
+						dyn.setContact(EntityDynamic.CONTACT_UP,true);
+					}
+					speed=speed.divide(2);
+					futurY.setPos(dyn.getPos().add(speed));
+					isColliding=collisionEE(futurY, collider);
+				}
+			}
+		}
+		
+		if(!collisionEE(futurY, collider)) {
+			dyn.setPos(futurY.getPos());
+		} else {
+			dyn.setSpeed(dyn.getSpeed().x,0);
+		}
+		if(Math.abs(dyn.getSpeed().y)>0.005) {
+			dyn.setContact(EntityDynamic.CONTACT_DOWN,false);
+			dyn.setContact(EntityDynamic.CONTACT_UP,false);
+		}
+	}
+	public static void adjustX(EntityDynamic dyn,ArrayList<Entity> entities) {
+		Entity futurX=new Entity();
+		futurX.setData(dyn.getData());
+		Vector2f speed=new Vector2f(dyn.getSpeed().x,0);
+		futurX.setPos(dyn.getPos().add(speed));
+		
+		int i=entities.indexOf(dyn);
+		
+		boolean isColliding = false;
+		Entity collider=null;
+		
+		for(int j=0;j<entities.size() && !isColliding;j++){
+			if(i!=j && entities.get(j).getCollide()) {
+				collider=entities.get(j);
+				isColliding=collisionEE(futurX, collider);
+				while(isColliding && Math.abs(speed.x)>0.005f) {
+					if(dyn.getPos().x > collider.getPos().x) {
+						dyn.setContact(EntityDynamic.CONTACT_RIGHT,true);
+						dyn.setContact(EntityDynamic.CONTACT_LEFT,false);
+					} else {
+						dyn.setContact(EntityDynamic.CONTACT_RIGHT,false);
+						dyn.setContact(EntityDynamic.CONTACT_LEFT,true);
+					} 
+					speed=speed.divide(2);
+					futurX.setPos(dyn.getPos().add(speed));
+					isColliding=collisionEE(futurX, collider);
+				}
+			}
+		}
+		
+		if(!isColliding) {
+			dyn.setPos(futurX.getPos());
+		} else {
+			dyn.setSpeed(0,speed.y);
+		}
+		if(Math.abs(dyn.getSpeed().x)>0.005) {
+			dyn.setContact(EntityDynamic.CONTACT_LEFT,false);
+			dyn.setContact(EntityDynamic.CONTACT_RIGHT,false);
+		}
+	}
+	public static final float GRAVITY = -0.0025f;
 }
