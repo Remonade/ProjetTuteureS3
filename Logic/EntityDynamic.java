9229bcf1f8b0291697bd34b08ae1c371c24b3033
Daratrixx <zeratul_du_38@hotmail.fr>
2015-11-19 18:16:08 +0100

    Update EntityDynamic.java

--- Logic/EntityDynamic.java
+++ Logic/EntityDynamic.java
@@ -6,7 +6,11 @@
 
 package Logic;
 
+import Graphic.GraphicMain;
+import Physic.PhysicMain;
+import java.util.ArrayList;
 import Maths.Vector2f;
+import Maths.Vector3f;
 
 public class EntityDynamic extends Entity {
 	public EntityDynamic() {
@@ -14,14 +18,7 @@ public class EntityDynamic extends Entity {
 		m_contact = new boolean[4];
 		for(boolean i:m_contact)
 			i=false;
-		m_speed=new Vector2f(0,0);
-	}
-	public EntityDynamic(float h,float w) {
-		super(h,w);
-		m_contact = new boolean[4];
-		for(boolean i:m_contact)
-			i=false;
-		m_speed=new Vector2f(0,0);
+		m_speed=new Vector2f();
 	}
 	public Vector2f getSpeed() {
 		return m_speed;
@@ -35,15 +32,18 @@ public class EntityDynamic extends Entity {
 	}
 	public void addSpeed(Vector2f speed) {
 		m_speed.add(speed);
+		if(Math.abs(m_speed.x)>maxspeed)
+			m_speed.x=maxspeed*Math.signum(m_speed.x)*0.25f;
+		if(Math.abs(m_speed.y)>maxspeed)
+			m_speed.y=maxspeed*Math.signum(m_speed.y);
 	}
 	public void addSpeed(float x,float y) {
 		m_speed.x+=x;
 		m_speed.y+=y;
-	}
-	@Override
-	public void delete() {
-		super.delete();
-		m_speed=null;
+		if(Math.abs(m_speed.x)>maxspeed)
+			m_speed.x=maxspeed*Math.signum(m_speed.x)*0.25f;
+		if(Math.abs(m_speed.y)>maxspeed)
+			m_speed.y=maxspeed*Math.signum(m_speed.y);
 	}
 	public boolean getContact(int contact) {
 		if(contact>-1 && contact<4)
@@ -54,10 +54,65 @@ public class EntityDynamic extends Entity {
 		if(contact>-1 && contact<4)
 			m_contact[contact]=value;
 	}
+	public void shoot(Vector2f target) {
+		EntityMissile mis=EntityMissile.create();
+		mis.setDir(target.subtract(m_pos).normalize());
+		mis.setPos(m_pos.add(mis.getDir().divide(50)));
+		mis.setData(EntityDataDynamic.get("missile"));
+		mis.setOwner(this);
+	}
+	public void jump() {
+		if(m_contact[CONTACT_DOWN])
+			setSpeed(0,0.075f);
+	}
+	public void damage(int damage) {
+		m_hp-=damage;
+	}
+	public int getHP() {
+		return m_hp;
+	}
+	public int getMaxHP() {
+		if(m_data==null)
+			return 0;
+		return ((EntityDataDynamic)m_data).getMaxHP();
+	}
+	public void update() {
+		addSpeed(0f,PhysicMain.GRAVITY);
+		if(m_hp<getMaxHP())
+			m_hp++;
+	}
+	@Override
+    public void draw() {
+		super.draw();
+		//GraphicMain.drawString(m_name,m_pos.add(m_size),0.01f,new Vector3f(1,1,1));
+		GraphicMain.drawString(getHP()+"/"+getMaxHP(),m_pos,0.01f,new Vector3f(1,1,1));
+	}
+	public boolean isActive() {
+		return m_active;
+	}
+	public void setActive(boolean value) {
+		m_active=value;
+	}
+	protected String m_name="Entity";
+	protected int m_hp=100;
 	protected Vector2f m_speed;
 	protected boolean m_contact[];
+	protected float maxspeed=0.1f;
+	protected boolean m_active=true;
+	
+	private static ArrayList<EntityDynamic> all=new ArrayList();
 	public static final int CONTACT_UP=0;
 	public static final int CONTACT_DOWN=1;
 	public static final int CONTACT_LEFT=2;
 	public static final int CONTACT_RIGHT=3;
+	
+	public static ArrayList getAll() {
+		return all;
+	}
+	public static EntityDynamic create() {
+		EntityDynamic temp=new EntityDynamic();
+		all.add(temp);
+		Entity.getAll().add(temp);
+		return temp;
+	}
 }
