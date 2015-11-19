483a7d083522280d39425acb0297bbf34ba12c46
Daratrixx <zeratul_du_38@hotmail.fr>
2015-11-19 18:58:44 +0100

    Update Camera2D.java

--- Graphic/Camera2D.java
+++ Graphic/Camera2D.java
@@ -12,7 +12,6 @@ package Graphic;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import Maths.Vector2f;
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
 import static org.lwjgl.opengl.GL11.GL_PROJECTION;
 import static org.lwjgl.opengl.GL11.glLoadIdentity;
 import static org.lwjgl.opengl.GL11.glMatrixMode;
@@ -21,7 +20,7 @@ import static org.lwjgl.opengl.GL11.glOrtho;
 
 
 public class Camera2D {
-	private float m_zoom=1, m_finalZoom=3;
+	private float m_zoom=3, m_finalZoom=3;
 	private float m_posX,m_posY;
 	private float m_BT,m_BB,m_BL,m_BR;
 	/**
@@ -41,15 +40,22 @@ public class Camera2D {
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		float ratio=(float)GraphicMain.WIDTH/(float)GraphicMain.HEIGHT;
+		//glTranslatef(-m_posX,-m_posY,0.0f);
+		//glFrustum(-m_zoom, m_zoom, -m_zoom/ratio, m_zoom/ratio, -10, 10);
+		
 		glTranslatef(-m_posX/m_zoom,-m_posY/m_zoom*ratio,0.0f);
 		glOrtho(-m_zoom, m_zoom, -m_zoom/ratio, m_zoom/ratio, -100, 100);
-		glMatrixMode(GL_MODELVIEW);
+	}
+	public void prepareGUICamera() {
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0,600,0,800, -100, 100);
 	}
 	public void update() {
 		if(m_zoom<m_finalZoom)
-			m_zoom+=0.05;
+			m_zoom+=0.075;
 		if(m_zoom>m_finalZoom)
-			m_zoom-=0.05;
+			m_zoom-=0.075;
 	}
 	public void setZoom(float zoom) {
 		if(zoom>1)
