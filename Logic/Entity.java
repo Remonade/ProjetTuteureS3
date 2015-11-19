c3b078bf0ecfbdb01eadba28956977d9ebfaf62e
Daratrixx <zeratul_du_38@hotmail.fr>
2015-11-19 18:15:25 +0100

    Update Entity.java

--- Logic/Entity.java
+++ Logic/Entity.java
@@ -1,49 +1,65 @@
 package Logic;
 
 import Graphic.Model;
+import java.util.ArrayList;
 import Maths.Vector2f;
 
 public class Entity {
 	public Entity() {
 		this.m_pos=new Vector2f(0,0);
-		this.m_size=new Vector2f(0,0);
 	}
-	public Entity(float h,float w) {
-		this.m_pos=new Vector2f(0,0);
-		this.m_size=new Vector2f(h,w);
+	public void setData(EntityData data) {
+		m_data=data;
+	}
+	public EntityData getData() {
+		return m_data;
 	}
 	public Vector2f getPos() {
 		return m_pos;
 	}
 	public void setPos(Vector2f pos) {
-		m_pos = pos;
+		m_pos.x = pos.x;
+		m_pos.y = pos.y;
 	}
 	public void setPos(float x,float y) {
 		m_pos.x=x;
 		m_pos.y=y;
 	}
 	public Vector2f getSize() {
-		return m_size;
+		if(m_data==null)
+				return new Vector2f(0.0f,0.0f);
+		return m_data.getSize();
 	}
-	public void setSize(Vector2f size) {
-		m_size = size;
-	}
-	public void setSize(float x,float y) {
-		m_size.x=x;
-		m_size.y=y;
-	}
-	public void delete() {
-		m_pos=null;
-		m_size=null;
-	}
-	public void setModel(Model m){
-		m_model = m;
+	public Model getModel() {
+		if(m_data==null)
+				return null;
+		return m_data.getModel();
 	}
 	public void draw() {
-		if(m_model!=null)
-			m_model.draw(m_pos,m_size);
+		if(getModel()!=null)
+			getModel().draw(m_pos,getSize());
 	}
-	protected Model m_model;
+	public boolean getCollide() {
+		return m_collide;
+	}
+	public boolean isVisible() {
+		return m_visible;
+	}
+	public void setVisible(boolean value) {
+		m_visible=value;
+	}
+	protected EntityData m_data;
 	protected Vector2f m_pos;
-	protected Vector2f m_size;
+	protected boolean m_collide=true;
+	private boolean m_visible=true;
+	private static ArrayList<Entity> all=new ArrayList();
+	
+	public static ArrayList getAll() {
+		return all;
+	}
+	public static Entity create() {
+		Entity temp=new Entity();
+		all.add(temp);
+		return temp;
+	}
 }
