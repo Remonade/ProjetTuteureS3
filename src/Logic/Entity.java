29131e4b3623ce94b9aa6db018b6624067a696e6
Dare <zeratul_du_38@hotmail.fr>
2015-11-21 16:01:48 +0100

    Ajout de la possibilité de tuer ->kill() pour les EntityDynamic

--- src/Logic/Entity.java
+++ src/Logic/Entity.java
@@ -5,61 +5,67 @@ import java.util.ArrayList;
 import Maths.Vector2f;
 
 public class Entity {
-    public Entity() {
-        this.m_pos=new Vector2f(0,0);
-    }
-    public void setData(EntityData data) {
-        m_data=data;
-    }
-    public EntityData getData() {
-        return m_data;
-    }
-    public Vector2f getPos() {
-        return m_pos;
-    }
-    public void setPos(Vector2f pos) {
-        m_pos.x = pos.x;
-        m_pos.y = pos.y;
-    }
-    public void setPos(float x,float y) {
-        m_pos.x=x;
-        m_pos.y=y;
-    }
-    public Vector2f getSize() {
-        if(m_data==null)
-            return new Vector2f(0.0f,0.0f);
-        return m_data.getSize();
-    }
-    public Model getModel() {
-        if(m_data==null)
-            return null;
-        return m_data.getModel();
-    }
-    public void draw() {
-        if(getModel()!=null)
-            getModel().draw(m_pos,getSize());
-    }
-    public boolean getCollide() {
-        return m_collide;
-    }
-    public boolean isVisible() {
-        return m_visible;
-    }
-    public void setVisible(boolean value) {
-        m_visible=value;
-    }
-    protected EntityData m_data;
-    protected Vector2f m_pos;
-    protected boolean m_collide=true;
-    private boolean m_visible=true;
-    private static ArrayList<Entity> all=new ArrayList();
-    
-    public static ArrayList getAll() {
-        return all;
-    }
-    public static Entity create() {
-        Entity temp=new Entity();
-        all.add(temp);
-        return temp;
-    }
+	public Entity() {
+		this.m_pos=new Vector2f(0,0);
+	}
+	public void setData(EntityData data) {
+		m_data=data;
+	}
+	public EntityData getData() {
+		return m_data;
+	}
+	public Vector2f getPos() {
+		return m_pos;
+	}
+	public void setPos(Vector2f pos) {
+		m_pos.x = pos.x;
+		m_pos.y = pos.y;
+	}
+	public void setPos(float x,float y) {
+		m_pos.x=x;
+		m_pos.y=y;
+	}
+	public Vector2f getSize() {
+		if(m_data==null)
+				return new Vector2f(0.0f,0.0f);
+		return m_data.getSize();
+	}
+	public Model getModel() {
+		if(m_data==null)
+				return null;
+		return m_data.getModel();
+	}
+	public void draw() {
+		if(getModel()!=null)
+			getModel().draw(m_pos,getSize());
+	}
+	public boolean getCollide() {
+		return m_collide;
+	}
+	public boolean isVisible() {
+		return m_visible;
+	}
+	public void setVisible(boolean value) {
+		m_visible=value;
+	}
+	protected EntityData m_data;
+	protected Vector2f m_pos;
+	protected boolean m_collide=true;
+	private boolean m_visible=true;
+	private static ArrayList<Entity> all=new ArrayList();
+	
+	public static ArrayList getAll() {
+		return all;
+	}
+	public static Entity create() {
+		Entity temp=new Entity();
+		add(temp);
+		return temp;
+	}
+	public static void add(Entity e) {
+		all.add(e);
+	}
+	public static void remove(Entity e) {
+		all.remove(e);
+	}
 }
--- src/Logic/EntityEnemy.java
+++ src/Logic/EntityEnemy.java
@@ -7,33 +7,42 @@ import Maths.Vector2f;
 public class EntityEnemy extends EntityDynamic {
     public Type m_type;
     public boolean m_orientation; //True means "look to the right"
-    
+
     /**
-     * Default constructor
-     */
+    * Default constructor
+    */
     public EntityEnemy() {
         super();
         m_type = MELEE;
         m_orientation = true;
-        m_name="Batard!";
+		m_name="Batard!";
     }
-    
+
     /**
-     * Complete constructor
-     */
+    * Complete constructor
+    */
     public EntityEnemy(Type m_type, Vector2f m_size, boolean m_orientation) {
-        super();
+        super();        
         this.m_type= m_type;
         this.m_orientation = m_orientation;
     }
-    
-    @Override
+
+	@Override
     public void update() {
-        super.update();
+		super.update();
         rotate();
         move();
-        
+		
     }
+	@Override
+	public void kill() {
+		remove(this);
+		
+		Entity temp;
+		temp=EntityEnemy.create();
+		temp.setData(EntityDataDynamic.get("enemy"));
+		temp.setPos(7,1);
+	}
     
     public void rotate() {
         if(m_pos.x > Logic.getPlayer().m_pos.x && m_orientation == true) {
@@ -46,33 +55,41 @@ public class EntityEnemy extends EntityDynamic {
     public void move() {
         switch(m_type){
             case MELEE :
-                if(Logic.getPlayer().getPos().y > m_pos.y+Logic.getPlayer().getSize().y)
-                    jump();
-                if(Math.abs(Logic.getPlayer().getPos().x-m_pos.x)>3f) {
-                    if(m_orientation) {
-                        setSpeed(0.05f, m_speed.y);
-                    }
-                    else {
-                        setSpeed(-0.05f, m_speed.y);
-                    }
-                    break;
-                }
+				if(Logic.getPlayer().getPos().y > m_pos.y+Logic.getPlayer().getSize().y)
+					jump();
+				if(Math.abs(Logic.getPlayer().getPos().x-m_pos.x)>2f) {
+					if(m_orientation) {
+						setSpeed(0.05f, m_speed.y);
+					}
+					else {
+						setSpeed(-0.05f, m_speed.y);
+					}
+				}
+				if(Math.random()<0.1)
+					shoot(Logic.getPlayer().getPos());
+				break;
             case RANGED :
-                shoot(Logic.getPlayer().getPos());
+				shoot(Logic.getPlayer().getPos());
                 break;
             default :
                 break;
         }
     }
-    public static EntityEnemy create() {
-        EntityEnemy temp=new EntityEnemy();
-        all.add(temp);
-        EntityDynamic.getAll().add(temp);
-        Entity.getAll().add(temp);
-        return temp;
-    }
-    public static ArrayList getAll() {
-        return all;
-    }
-    private static ArrayList<EntityEnemy> all=new ArrayList();
+	public static EntityEnemy create() {
+		EntityEnemy temp=new EntityEnemy();
+		add(temp);
+		return temp;
+	}
+	public static void add(EntityEnemy e) {
+		EntityDynamic.add(e);
+		all.add(e);
+	}
+	public static void remove(Entity e) {
+		EntityDynamic.remove(e);
+		all.remove(e);
+	}
+	public static ArrayList getAll() {
+		return all;
+	}
+	private static ArrayList<EntityEnemy> all=new ArrayList();
 }
