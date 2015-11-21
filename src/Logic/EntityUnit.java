6f04e583561f08224df82d1b179f804a7de4f4d3
Daratrixx <zeratul_du_38@hotmail.fr>
2015-11-21 17:49:05 +0100

    Update and rename EntityEnemy.java to EntityUnit.java

--- src/Logic/EntityUnit.java
+++ src/Logic/EntityUnit.java
@@ -1,17 +1,20 @@
 package Logic;
 
+import Graphic.GraphicMain;
 import static Logic.Type.MELEE;
 import java.util.ArrayList;
 import Maths.Vector2f;
+import Maths.Vector3f;
 
-public class EntityEnemy extends EntityDynamic {
+public class EntityUnit extends EntityDynamic {
     public Type m_type;
     public boolean m_orientation; //True means "look to the right"
+	protected int m_health=0;
 
     /**
     * Default constructor
     */
-    public EntityEnemy() {
+    public EntityUnit() {
         super();
         m_type = MELEE;
         m_orientation = true;
@@ -21,27 +24,63 @@ public class EntityEnemy extends EntityDynamic {
     /**
     * Complete constructor
     */
-    public EntityEnemy(Type m_type, Vector2f m_size, boolean m_orientation) {
-        super();        
+    public EntityUnit(Type m_type, Vector2f m_size, boolean m_orientation) {
+        super();
         this.m_type= m_type;
         this.m_orientation = m_orientation;
     }
-
+	@Override
+	public void setData(EntityData data) {
+		super.setData(data);
+		m_health=getMaxHealth();
+	}
+	public int getHealth() {
+		return m_health;
+	}
+	public int getMaxHealth() {
+		if(m_data==null)
+			return 0;
+		return ((EntityDataUnit)m_data).getMaxHealth();
+	}
 	@Override
     public void update() {
 		super.update();
+		//if(m_health<getMaxHealth())
+			//m_hp++;
+		if(m_health>getMaxHealth())
+			m_health=getMaxHealth();
         rotate();
         move();
 		
     }
-	@Override
 	public void kill() {
 		remove(this);
 		
-		Entity temp;
-		temp=EntityEnemy.create();
-		temp.setData(EntityDataDynamic.get("enemy"));
-		temp.setPos(7,1);
+		if(this==Logic.getPlayer())
+			Logic.killPlayer();
+		else {
+			Entity temp;
+			temp=EntityUnit.create();
+			temp.setData(EntityDataUnit.get("enemy"));
+			temp.setPos(7,1);
+		}
+	}
+	public void shoot(Vector2f target) {
+		EntityMissile mis=EntityMissile.create();
+		mis.setDir(target.subtract(m_pos).normalize());
+		mis.setPos(m_pos.add(mis.getDir().divide(50)));
+		mis.setData(EntityDataUnit.get("missile"));
+		mis.setOwner(this);
+		mis.setTeam(getTeam());
+	}
+	public void jump() {
+		if(m_contact[CONTACT_DOWN])
+			setSpeed(0,0.075f);
+	}
+	public void damage(int damage) {
+		m_health-=damage;
+		if(m_health<=0)
+			kill();
 	}
     
     public void rotate() {
@@ -75,12 +114,17 @@ public class EntityEnemy extends EntityDynamic {
                 break;
         }
     }
-	public static EntityEnemy create() {
-		EntityEnemy temp=new EntityEnemy();
+	@Override
+    public void draw() {
+		super.draw();
+		GraphicMain.drawString(getHealth()+"/"+getMaxHealth(),m_pos.add(getSize()),0.01f,new Vector3f(1,1,1));
+	}
+	public static EntityUnit create() {
+		EntityUnit temp=new EntityUnit();
 		add(temp);
 		return temp;
 	}
-	public static void add(EntityEnemy e) {
+	public static void add(EntityUnit e) {
 		EntityDynamic.add(e);
 		all.add(e);
 	}
@@ -91,5 +135,5 @@ public class EntityEnemy extends EntityDynamic {
 	public static ArrayList getAll() {
 		return all;
 	}
-	private static ArrayList<EntityEnemy> all=new ArrayList();
+	private static ArrayList<EntityUnit> all=new ArrayList();
 }
